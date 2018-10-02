b1d015389643b372fc4b8282973c0afcf34c67ea
Aram Kocharyan <akarmenia@gmail.com>
2018-10-02 09:28:11 -0700

    Update to Meteor 1.6.1

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 // Meteor package definition.
 Package.describe({
   name: 'aramk:jqtree',
-  version: '1.0.0',
+  version: '2.0.0',
   summary: 'jQuery tree widget',
   git: 'https://github.com/aramk/jqTree.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.0');
+  api.versionsFrom('METEOR@1.6.1');
   api.use([
     'coffeescript',
     'jquery',
